93c84831ee138075fca4a9537d08d11349805c60
Peca159k <romapetrenko19@gmail.com>
2019-09-10 20:45:54 +0300

    +add: root component transferred to hooks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React, {Component} from "react";
+import React, {Component, useEffect} from "react";
 import {connect} from "react-redux";
 import {startApp} from "./action/app";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "./container/Home";
 
-@connect(state => state, {startApp})
+const App = props => {
+  const {startApp} = props
+  useEffect(() => {
+    startApp()
+  }, [])
 
-class App extends Component {
-
-    componentWillMount(){
-        this.props.startApp()
-    }
-
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="*" render={() => (
-                        <div><h1>404 Not found!</h1></div>
-                    )}/>
-                </Switch>
-            </Router>
-        )
-    }
+  return (
+     <Router>
+            <Switch>
+                c<Route exact path="/" component={Home}/>
+                <Route path="*" render={() => (
+                    <div><h1>404 Not found!</h1></div>
+                )}/>
+           </Switch>
+     </Router>
+  )
 }
 
-export default App
+const mapDispatchToProps = (dispatch) => {
+  return ({
+    startApp
+  })
+}
+
+export default connect(null, mapDispatchToProps)(App)
