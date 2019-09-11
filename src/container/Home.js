253e3f67676e0c539838ea4d317210b6e90f496c
Peca159k <romapetrenko19@gmail.com>
2019-09-11 10:02:04 +0300

    ~update: packages updated

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,27 +1,40 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import {__Home__} from "./styled";
+import React from "react";
+import { connect } from "react-redux";
+import { __Home__ } from "./styled";
 
 const Home = props => {
-  const {articles} = props
-    return (
-        <__Home__>
-           {articles && articles.length && articles.map(item => {
-             const {created_at, description, id, name, price, status, updated_at} = item
-             return(
-               <div className={'articles-item'}>
-                <p>{description}</p>
-                <p>{name}</p>
-                <p>{price}</p>
-               </div>
-             )
-           })}
-        </__Home__>
-    )
-}
+  const { articles } = props;
+  return (
+    <__Home__>
+      {articles &&
+        articles.length &&
+        articles.map(item => {
+          const {
+            created_at,
+            description,
+            id,
+            name,
+            price,
+            status,
+            updated_at
+          } = item;
+          return (
+            <div className={"articles-item"}>
+              <p>{description}</p>
+              <p>{name}</p>
+              <p>{price}</p>
+            </div>
+          );
+        })}
+    </__Home__>
+  );
+};
 
 const mapStateToProps = state => ({
   articles: state.articles
-})
+});
 
-export default connect(mapStateToProps, null)(Home)
+export default connect(
+  mapStateToProps,
+  null
+)(Home);
